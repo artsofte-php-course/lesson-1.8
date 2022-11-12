2bc5b3f67e8f83d3ffd28fa05bb46649d4e1aefd
Max Selezenev <selezenev@gmail.com>
2022-11-12 12:33:33 +0500

    lesson8 code

--- BaseProject.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class BaseProject {
-	
-	protected $id = null;
-	protected $name = null;
-
-}
--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php 
 
-require_once 'BaseProject.php';
-require_once 'Project.php';
-require_once 'Task.php';
+use Repository\ProjectRepository;
+
+spl_autoload_register(function($className) {
+    
+    $path = __DIR__ . DIRECTORY_SEPARATOR . 
+                    str_replace("\\", DIRECTORY_SEPARATOR, $className) . ".php";
+    require_once $path;
+});
 
 
-$project = new Project("Make a project");
+
+$projectRepository = new ProjectRepository([
+        "Make a project one", 
+        "Make a project two", 
+        "Another project"
+    ]
+);
+
+$project =  $projectRepository->findById(1);
 $project->addTask("Create repository");
 $project->addTask("Code");
 $project->addTask("Code...");
@@ -14,13 +27,10 @@ $project->addTask("Push into repo, make pull request");
 
 
 $project2 = new Project("Project 2");
+$projectRepository->add($project2);
 
-var_dump($project2);
-
-
-
-
-
+$projectCnt = count($projectRepository);
+var_dump($projectCnt);
 
 
 
